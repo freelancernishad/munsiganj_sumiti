8bfd5dbdbd921e45154ef04de6fec811ba6213a9
Softlabltd.com <freelancernishad123@gmail.com>
2022-05-08 11:48:17 +0600

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+
     }
 
     /**
@@ -25,7 +26,7 @@ class HomeController extends Controller
     {
         return redirect('admin/members');
     }
-    
-    
+
+
 
 }
