c46573141eec64159a5a1818661e4a60056679c0
Softlabltd.com <freelancernishad123@gmail.com>
2022-06-22 15:17:21 +0600

    committee image

--- resources/views/admin/committee/list.blade.php
+++ resources/views/admin/committee/list.blade.php
@@ -20,10 +20,12 @@
             <div class="x_content">
                 <div class="row">
                     <div class="col-sm-12">
-                        @if($type=='')
-                        <a href="{{ route('active.committees.excel') }}" class="btn btn-danger float-right" >Download Excel Sheet</a>
-                        @elseif($type=='ex')
-                        <a href="{{ route('ex.committees.excel') }}" class="btn btn-danger float-right" >Download Excel Sheet</a>
+                        @if ($type == '')
+                            <a href="{{ route('active.committees.excel') }}" class="btn btn-danger float-right">Download
+                                Excel Sheet</a>
+                        @elseif($type == 'ex')
+                            <a href="{{ route('ex.committees.excel') }}" class="btn btn-danger float-right">Download Excel
+                                Sheet</a>
                         @endif
                         <div class="card-box table-responsive">
                             <table id="datatable-checkbox" class="table table-striped table-bordered bulk_action"
@@ -42,39 +44,35 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-@php
-    $i = 1;
-    @endphp
+                                    @php
+                                        $i = 1;
+                                    @endphp
                                     @foreach ($rows as $row)
+                                        <tr>
+                                            <td>{{ $i++ }}</td>
+                                            <td>{{ $row->name }}</td>
+                                            <td>{{ $row->dob }}</td>
+                                            <td>{{ $row->p_vill }} ,{{ $row->p_post }} ,{{ $row->p_thana }}
+                                                ,{{ $row->p_dist }}</td>
+                                            <td>{{ $row->session_start }}</td>
+                                            <td>{{ date('Y-m-d h:i:s A', strtotime($row->created_at)) }} </td>
+                                            <td><img width="100%" src="{{ asset($row->image) }}" alt="" /></td>
+                                            <td>
+                                                <a href="{{ route('committee.edit', ['committee' => $row->id]) }}"
+                                                    class="btn btn-info">Edit</a>
+
+
+                                                <form method="post"
+                                                    action="{{ route('committee.destroy', ['committee' => $row->id]) }}">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                </form>
 
 
 
-
-                                    <tr>
-                                        <td>{{ $i++ }}</td>
-                                        <td>{{ $row->name }}</td>
-                                        <td>{{ $row->dob }}</td>
-                                        <td>{{ $row->p_vill }} ,{{ $row->p_post }} ,{{ $row->p_thana }} ,{{ $row->p_dist }}</td>
-                                        <td>{{ $row->session_start }}</td>
-                                        <td>{{ date("Y-m-d h:i:s A", strtotime($row->created_at)) }} </td>
-                                        <td><img width="100%" src="{{ $row->image }}" alt="" /></td>
-                                        <td>
-                                            <a href="{{ route('committee.edit', ['committee' => $row->id]) }}" class="btn btn-info">Edit</a>
-
-
-                                            <form method="post" action="{{ route('committee.destroy', ['committee' => $row->id]) }}">
-                                                @csrf
-                                                @method('delete')
-                                                <button type="submit" class="btn btn-danger">Delete</button>
-                                            </form>
-
-
-
-                                        </td>
-                                    </tr>
-
-
-
+                                            </td>
+                                        </tr>
                                     @endforeach
 
 
